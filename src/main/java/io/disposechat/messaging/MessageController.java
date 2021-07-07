2b42e429b0788ce8bce10513cf769d1881c10377
Oliver Broughton <olibrougthon@gmail.com>
2021-07-07 16:06:05 +0100

    Added reactive RedisPubSub

--- src/main/java/io/disposechat/messaging/MessageController.java
+++ src/main/java/io/disposechat/messaging/MessageController.java
@@ -1,61 +1,65 @@
 package io.disposechat.messaging;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import lombok.extern.slf4j.XSlf4j;
-import org.apache.commons.lang3.SerializationUtils;
+import org.springframework.data.redis.connection.ReactiveSubscription;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.ReactiveRedisMessageListenerContainer;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.messaging.rsocket.annotation.ConnectMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.SerializationUtils;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import reactor.rabbitmq.OutboundMessage;
-import reactor.rabbitmq.Receiver;
-import reactor.rabbitmq.Sender;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 
 @Controller
 @Slf4j
 public class MessageController {
 
-    private final Sender sender;
-    private final Receiver receiver;
-    private final String queue;
-    private final String exchange;
+    private final ReactiveRedisTemplate<String, Message> reactiveTemplate;
+    private final ChannelTopic messageTopic;
+    private final ReactiveRedisMessageListenerContainer reactiveMsgListenerContainer;
 
-    public MessageController(Sender sender, Receiver receiver, String queue, String exchange) {
-        this.sender = sender;
-        this.receiver = receiver;
-        this.queue = queue;
-        this.exchange = exchange;
+    public MessageController(ReactiveRedisTemplate<String, Message> reactiveTemplate,
+                             ReactiveRedisMessageListenerContainer reactiveMsgListenerContainer,
+                             ChannelTopic messageTopic) {
+
+        this.reactiveMsgListenerContainer = reactiveMsgListenerContainer;
+        this.reactiveTemplate = reactiveTemplate;
+        this.messageTopic = messageTopic;
     }
 
 //    @MessageMapping("send")
 //    void send(Flux<Message> messages) {
 //        messages.map(message -> new OutboundMessage(exchange, "", SerializationUtils.serialize(message)))
-//                .flatMap(outboundMessage -> this.sender.send(Mono.just(outboundMessage)))
-//                .subscribe();
 //    }
 
     @ConnectMapping
     void onConnect(RSocketRequester requester)
     {
+        Objects.requireNonNull(requester.rsocket()).onClose()
+                .subscribe((close) -> log.info("disconnected" +  requester.rsocketClient()));
+
         log.info("connected" +  requester.rsocketClient());
     }
 
     @MessageMapping("send")
     void send( RSocketRequester requester, Message message) {
-        log.info(requester.rsocketClient().toString());
-        this.sender.send(Mono.just(new OutboundMessage(exchange, "", SerializationUtils.serialize(message))))
-                .subscribe();
+        this.reactiveTemplate.convertAndSend(messageTopic.getTopic(), message).subscribe();
     }
 
     @MessageMapping("receive")
     Flux<Message> receive() {
-
-
-
-        // todo - look into json serialization / wrappers
-        return this.receiver.consumeAutoAck(queue)
-                .map(delivery -> SerializationUtils.deserialize(delivery.getBody()));
+        List<ChannelTopic> topicList = Collections.singletonList(messageTopic);
+        return reactiveMsgListenerContainer
+                .receive(topicList, reactiveTemplate.getSerializationContext().getKeySerializationPair(),
+                            reactiveTemplate.getSerializationContext().getValueSerializationPair())
+                .map(ReactiveSubscription.Message::getMessage);
     }
 }
---
+++ src/main/java/io/disposechat/messaging/ReactiveRedisConfig.java
@@ -0,0 +1,55 @@
+package io.disposechat.messaging;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.ReactiveRedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class ReactiveRedisConfig {
+
+    @Bean
+    ChannelTopic topic() {
+        return new ChannelTopic("message:broadcast");
+    }
+
+
+    @Bean
+    public ReactiveRedisTemplate<String, Message> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+
+        StringRedisSerializer keySerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer<Message> valueSerializer = new Jackson2JsonRedisSerializer<>(Message.class);
+
+        RedisSerializationContext.RedisSerializationContextBuilder<String, Message> builder =
+                RedisSerializationContext.newSerializationContext(keySerializer);
+
+        RedisSerializationContext<String, Message> context =
+                builder.value(valueSerializer).build();
+
+        return new ReactiveRedisTemplate<>(factory, context);
+    }
+
+    @Bean
+    ReactiveRedisMessageListenerContainer container(ReactiveRedisConnectionFactory factory) {
+        ReactiveRedisMessageListenerContainer container = new ReactiveRedisMessageListenerContainer(factory);
+        container.receive(topic());
+        return container;
+    }
+
+    @Bean
+    ReactiveRedisOperations<String, Message> redisOperations(ReactiveRedisConnectionFactory factory) {
+        Jackson2JsonRedisSerializer<Message> serializer = new Jackson2JsonRedisSerializer<>(Message.class);
+
+        RedisSerializationContext.RedisSerializationContextBuilder<String, Message> builder =
+                RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
+
+        RedisSerializationContext<String, Message> context = builder.value(serializer).build();
+        return new ReactiveRedisTemplate<>(factory, context);
+    }
+}
