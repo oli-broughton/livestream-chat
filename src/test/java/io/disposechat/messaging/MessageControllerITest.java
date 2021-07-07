7acb2830014729dac6a5524a26d04eb7895d3676
Oliver Broughton <olibrougthon@gmail.com>
2021-07-07 20:48:58 +0100

    integration test for sending messages now working my publishing the send request on a new thread

--- src/test/java/io/disposechat/messaging/MessageControllerITest.java
+++ src/test/java/io/disposechat/messaging/MessageControllerITest.java
@@ -7,12 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.messaging.rsocket.RSocketRequester;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Hooks;
-import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
-
-import java.time.Duration;
 
 @SpringBootTest
 class MessageControllerITest {
@@ -21,6 +18,7 @@ class MessageControllerITest {
 
     @BeforeAll
     public static void setupUp(@Autowired RSocketRequester.Builder builder, @Value("${spring.rsocket.server.port}") Integer port) {
+
         //https://github.com/rsocket/rsocket-java/issues/1018
         Hooks.onErrorDropped((throwable) -> {
         });
@@ -38,14 +36,11 @@ class MessageControllerITest {
         var receiveRoute = "receive";
         var message = new Message("test", "test", "test");
 
-        Flux<Message> send = Flux.interval(Duration.ofMillis(100)).map(aLong -> message);
 
-        Flux<Message> result = requester
-                .route(receiveRoute)
-                .data(send)
+        var messages = requester.route(receiveRoute)
                 .retrieveFlux(Message.class);
 
-
-        StepVerifier.create(result).thenConsumeWhile(message1 -> true, System.out::println).expectNextCount(10).thenCancel().verify();
+        requester.route(sendRoute).data(message).send().subscribeOn(Schedulers.single()).subscribe();
+        StepVerifier.create(messages).consumeNextWith(System.out::println).thenCancel().verify();
     }
 }
