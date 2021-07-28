4089d5383b302b9d379c8c5e41a19eb8043f092a
Oliver Broughton <olibrougthon@gmail.com>
2021-07-28 21:17:14 +0100

    Optimized imports

--- src/main/java/io/disposechat/user/RedisUserStoreConfig.java
+++ src/main/java/io/disposechat/user/RedisUserStoreConfig.java
@@ -1,6 +1,5 @@
 package io.disposechat.user;
 
-import io.disposechat.messaging.Message;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
