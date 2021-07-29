b2f748023bd5ad4800d5c31be94235e785b3d5ab
Oliver Broughton <olibrougthon@gmail.com>
2021-07-29 20:32:30 +0100

    refactored UserRepository interface to find by the username key

--- src/main/java/io/disposechat/user/UserRepository.java
+++ src/main/java/io/disposechat/user/UserRepository.java
@@ -2,9 +2,8 @@ package io.disposechat.user;
 
 import reactor.core.publisher.Mono;
 
-public interface UserRepository{
-
+public interface UserRepository {
     Mono<User> add(String name);
-    Mono<User> findById(String Id);
 
+    Mono<User> findByUsername(String username);
 }
