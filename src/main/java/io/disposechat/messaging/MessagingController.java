fe76a10d2efd02cbbd77d35855050dd27b54e07e
Oliver Broughton <olibrougthon@gmail.com>
2021-08-10 10:20:43 +0100

    optimised imports

--- src/main/java/io/disposechat/messaging/MessagingController.java
+++ src/main/java/io/disposechat/messaging/MessagingController.java
@@ -2,6 +2,7 @@ package io.disposechat.messaging;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.messaging.rsocket.annotation.ConnectMapping;
